8423c21c819d823256d8b8e1c170b64f73a5ce42
unknown <mislav.sagovac@contentio.biz>
2023-07-05 13:55:54 +0200

    init

--- download.R
+++ download.R
@@ -4,4 +4,4 @@ blob_key = readLines('./blob_key.txt')
 endpoint = "https://snpmarketdata.blob.core.windows.net/"
 BLOBENDPOINT = storage_endpoint(endpoint, key=blob_key)
 cont = storage_container(BLOBENDPOINT, "jphd")
-storage_download(cont, "pead-predictors-sample.csv", overwrite=T)
+storage_download(cont, "pead-predictors-sample.csv", overwrite=TRUE)
